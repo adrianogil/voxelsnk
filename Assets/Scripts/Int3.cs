71b8e496deb4af1d7ef611f292024942b41b5280
Adriano Gil <adrianogil.san@gmail.com>
2018-12-05 10:46:40 -0400

    Snake movement in grid

--- Assets/Scripts/Int3.cs
+++ Assets/Scripts/Int3.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 
 public struct Int3
 {
@@ -10,10 +11,36 @@ public struct Int3
         this.z = z;
     }
 
+    public int ManhattanDistance(Int3 b)
+    {
+        return Mathf.Abs(this.x - b.x) +
+               Mathf.Abs(this.y - b.y) +
+               Mathf.Abs(this.z - b.z);
+    }
+
     public static Int3 operator%(Int3 a, int b) { return new Int3(a.x % b, a.y % b, a.z % b); }
     public static Int3 operator%(Int3 a, Int3 b) { return new Int3(a.x % b.x, a.y % b.y, a.z % b.z); }
 
     public static Int3 operator+(Int3 a, Int3 b) { return new Int3(a.x + b.x, a.y + b.y, a.z + b.z); }
         // Subtracts one vector from another.
     public static Int3 operator-(Int3 a, Int3 b) { return new Int3(a.x - b.x, a.y - b.y, a.z - b.z); }
+
+    public static bool operator==(Int3 a, Int3 b) { return  a.x == b.x && a.y == b.y && a.z == b.z; }
+    public static bool operator!=(Int3 a, Int3 b) { return  !(a.x == b.x && a.y == b.y && a.z == b.z); }
+
+    public static Int3 operator*(Int3 a, int b) { return new Int3(a.x * b, a.y * b, a.z * b); }
+    public static Int3 operator*(int b, Int3 a) { return new Int3(a.x * b, a.y * b, a.z * b); }
+
+    public static Int3 one() { return new Int3(1,1,1); }
+    public static Int3 zero() { return new Int3(0,0,0); }
+
+    public static Int3 random(Int3 a, Int3 b) {
+        Int3 int3Value = new Int3(0,0,0);
+
+        int3Value.x = Random.Range(a.x, b.x);
+        int3Value.y = Random.Range(a.y, b.y);
+        int3Value.z = Random.Range(a.z, b.z);
+
+        return int3Value;
+    }
 }
--- Assets/Scripts/VoxelSnake.cs
+++ Assets/Scripts/VoxelSnake.cs
@@ -1,4 +1,17 @@
 using UnityEngine;
+using UnityEditor;
+
+using System;
+
+public enum VoxelSnakeDirection
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Forward,
+    Backward
+}
 
 [RequireComponent(typeof(ImageSetter))]
 public class VoxelSnake : MonoBehaviour
@@ -16,6 +29,7 @@ public class VoxelSnake : MonoBehaviour
     [Header("Game Elements")]
     public Color backgroundColor;
     public Color playerColor;
+    public Color foodColor;
 
     private ImageSetter mImageSetter;
 
@@ -24,18 +38,14 @@ public class VoxelSnake : MonoBehaviour
     private int mLastX, mLastY, mLastZ;
     private int mTailX, mTailY, mTailZ;
 
-    private Int3 mCurrent, mDelta, mLast, mTail, mLastDirection;
+    private Int3 mCurrent, mDelta, mLast, mTail, mLastDirection, mLastFoodPosition = -1 * Int3.one(), mWorldSize;
 
     private float lastMovementUpdate = 0f;
 
-    void Start ()
+    public VoxelSnakeDirection currentSnakeDirection = VoxelSnakeDirection.Forward;
+
+    void ResetVoxelGridToBackgroundColor()
     {
-        mImageSetter = GetComponent<ImageSetter>();
-
-        mCurrent.x = initialPlayerPositionX;
-        mCurrent.y = initialPlayerPositionY;
-        mCurrent.z = initialPlayerPositionZ;
-
         for (int x = 0; x < worldSizeX; x++)
         {
             for (int y = 0; y < worldSizeY; y++)
@@ -46,52 +56,162 @@ public class VoxelSnake : MonoBehaviour
                 }
             }
         }
+    }
 
+    void Start ()
+    {
+        mImageSetter = GetComponent<ImageSetter>();
+
+        mCurrent.x = initialPlayerPositionX;
+        mCurrent.y = initialPlayerPositionY;
+        mCurrent.z = initialPlayerPositionZ;
+
+        ResetVoxelGridToBackgroundColor();
+
+        // Set initial player position
         mImageSetter.SetPixelColor(mCurrent.x + mCurrent.z*worldSizeX, mCurrent.y, playerColor);
 
         mLastDirection = new Int3(0, 0, 1);
+
+        mCurrent = new Int3(mCurrentX, mCurrentY, mCurrentZ);
+        mWorldSize = new Int3(worldSizeX, worldSizeY, worldSizeZ);
+
+        PlaceFoodAtRandom();
     }
 
     // Update is called once per frame
     void Update ()
     {
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            mDelta = new Int3(0,0,1);
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            mDelta = new Int3(0,0,-1);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            mDelta = new Int3(-1,0,0);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            mDelta = new Int3(1,0,0);
-        }
-        if (Input.GetKeyUp(KeyCode.Q))
-        {
-            mDelta = new Int3(0,-1,0);
-        }
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            mDelta = new Int3(0,1,0);
-        }
-
         if (Time.time - lastMovementUpdate > movementTime)
         {
+            UpdateAI();
             UpdateMovement();
+            lastMovementUpdate = Time.time;
         }
+    }
+
+    Int3 GetDeltaByDirection(VoxelSnakeDirection dir)
+    {
+        if (dir == VoxelSnakeDirection.Forward)
+        {
+            return new Int3(0,0,1);
+        }
+        else if (dir == VoxelSnakeDirection.Backward)
+        {
+            return new Int3(0,0,-1);
+        }
+        else if (dir == VoxelSnakeDirection.Left)
+        {
+            return new Int3(-1,0,0);
+        }
+        else if (dir == VoxelSnakeDirection.Right)
+        {
+            return new Int3(1,0,0);
+        }
+        else if (dir == VoxelSnakeDirection.Down)
+        {
+            return new Int3(0,-1,0);
+        }
+        else if (dir == VoxelSnakeDirection.Up)
+        {
+            return new Int3(0,1,0);
+        }
+
+        // Up
+        return new Int3(0,1,0);
     }
 
     void UpdateMovement()
     {
-        // mCurrent = (mCurrent + mDelta) % mWorldSize;
+        mDelta = GetDeltaByDirection(currentSnakeDirection);
 
-        mImageSetter.SetPixelColor(mLastX + mLastZ*worldSizeX, mLastY, backgroundColor);
-        mImageSetter.SetPixelColor(mCurrentX + mCurrentZ*worldSizeX, mCurrentY, playerColor);
+        SetVoxelColor(mCurrent, backgroundColor);
+        mCurrent = (mCurrent + mDelta) % mWorldSize;
+        SetVoxelColor(mCurrent, playerColor);
+
+        // mImageSetter.SetPixelColor(mLastX + mLastZ*worldSizeX, mLastY, backgroundColor);
+        // mImageSetter.SetPixelColor(mCurrentX + mCurrentZ*worldSizeX, mCurrentY, playerColor);
+    }
+
+
+
+    void UpdateAI()
+    {
+        if (mCurrent == mLastFoodPosition)
+        {
+            PlaceFoodAtRandom();
+        }
+
+        float bestUtilityValue = int.MaxValue;
+        VoxelSnakeDirection bestDirection = VoxelSnakeDirection.Up;
+
+        float utilityValue = 0;
+        Int3 delta;
+
+        foreach(VoxelSnakeDirection dir in Enum.GetValues(typeof(VoxelSnakeDirection)))
+        {
+            delta = GetDeltaByDirection(dir);
+            utilityValue = mLastFoodPosition.ManhattanDistance((mCurrent+delta)%mWorldSize);
+
+            if (utilityValue < bestUtilityValue)
+            {
+                bestUtilityValue = utilityValue;
+                bestDirection = dir;
+            }
+        }
+
+        currentSnakeDirection = bestDirection;
+    }
+
+    public void PlaceFoodAtRandom()
+    {
+        mWorldSize = new Int3(worldSizeX, worldSizeY, worldSizeZ);
+
+        if (mLastFoodPosition != (-1*Int3.one()))
+        {
+            Debug.Log("GilLog - VoxelSnake::PlaceFoodAtRandom " + GetVoxelColor(mLastFoodPosition));
+            Vector4 vColor = GetVoxelColor(mLastFoodPosition);
+            Vector4 fColor = foodColor;
+            if ((vColor - fColor).sqrMagnitude < 0.001)
+            {
+                Debug.Log("GilLog - VoxelSnake::PlaceFoodAtRandom - Lets reset food color in last pos");
+                SetVoxelColor(mLastFoodPosition, backgroundColor);
+            }
+        }
+
+        mLastFoodPosition = Int3.random(Int3.zero(), mWorldSize);
+        SetVoxelColor(mLastFoodPosition, foodColor);
+    }
+
+    public void SetVoxelColor(Int3 pos, Color color)
+    {
+        mImageSetter.SetPixelColor(pos.x + pos.z*worldSizeX, pos.y, color);
+    }
+
+    public Color GetVoxelColor(Int3 pos)
+    {
+        return mImageSetter.GetPixelColor(pos.x + pos.z*worldSizeX, pos.y);
     }
 
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(VoxelSnake))]
+public class VoxelSnakeEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        VoxelSnake editorObj = target as VoxelSnake;
+
+        if (editorObj == null) return;
+
+        if (GUILayout.Button("Place Food At Random"))
+        {
+            editorObj.PlaceFoodAtRandom();
+        }
+    }
+
+}
+#endif
